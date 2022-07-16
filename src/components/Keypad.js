e2179eb649ba91ab0fa478d3884c3c404afe4055
alemba-21 <alembakiragu@gmail.com>
2022-07-16 23:45:28 +0300

    commiting changes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function dealWithChange(){
+        console.log("Entering password...")
+    }
+
+    return(
+        <input type="password" onChange={dealWithChange} />
+    )
+}
+
+export default Keypad;
